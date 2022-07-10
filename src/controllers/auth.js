fa75b9c238a7e77de923bbe2627d7f6fac05a4fa
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-10 15:33:56 +0530

    Remove Exception handling

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,21 +2,14 @@ import asyncHandler from "../middleware/async"
 import {authRegister} from "../services/auth"
 import {makeResponse} from "../utils/response"
 import {sendTokenResponse} from "../utils/jwt";
-import logger from "../utils/logger";
 
 export const register = asyncHandler(async(req, res, next) => {
-    try {
-        const {name, email, password, photo_url} = req.body;
-        
-        const user = await authRegister(name, email, password, photo_url);
+        const {name, email, password, university} = req.body;
+        const user = await authRegister(name, email, password, university);
         if (user) return makeResponse({res, message:"User created successfully"});
         return makeResponse({res, status: 500, message:"Failed to register user"});
     }
-    catch (e) {
-        logger.error(e);
-        return makeResponse({res, status:500,  message:e.message});
-    }
-});
+);
 
 export const login = asyncHandler(async(req, res, next) => {})
 
