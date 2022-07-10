8e6fc2a94950e8ec63939c6001b6302c7d69d28b
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-10 20:43:30 +0530

    Implement getone user function

--- src/repository/user.js
+++ src/repository/user.js
@@ -3,3 +3,7 @@ import User from "../models/user";
 export const createUser = async (user) => {
     return await new User(user).save();
 }
+
+export const getOneUser = async (email) => {
+    return await User.findOne({email:email});
+}
