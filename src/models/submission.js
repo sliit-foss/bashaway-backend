5caec3126f4d8b7a305a601ff38265b636532dd7
Shifan <shifan611@gmail.com>
2022-07-10 17:23:47 +0530

    Added timestamps to new submissions

--- src/models/submission.js
+++ src/models/submission.js
@@ -1,22 +1,28 @@
 import mongoose from "mongoose";
 import mongoosePaginate from 'mongoose-paginate-v2';
 
-const SubmissionSchema = mongoose.Schema({
-    group: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Group',
-        required: true
+const SubmissionSchema = mongoose.Schema(
+    {
+        group: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Group',
+            required: true
+        },
+        question: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Question',
+            required: true
+        },
+        link: {
+            type: String,
+            required: true
+        }
     },
-    question: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Question',
-        required: true
-    },
-    link: {
-        type: String,
-        required: true
+    {
+        versionKey: false,
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
     }
-})
+)
 
 SubmissionSchema.plugin(mongoosePaginate);
 
--- src/routes/submission.routes.js
+++ src/routes/submission.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { create } from '../controllers/submission';
 
-const authRouter = express.Router();
+const submissionRouter = express.Router();
 
-authRouter.post('/create', create);
+submissionRouter.post('/create', create);
 
-export default authRouter;
+export default submissionRouter;
