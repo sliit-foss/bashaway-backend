0d28e06f28cdb424949c25c707f1cb130a0d0a2a
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-10 20:44:19 +0530

    Implement authLogin service function

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,9 @@
 import bcrypt from 'bcrypt';
-import {createUser} from "../repository/user"
+import {createUser, getOneUser} from "../repository/user"
 import { v4 as uuidv4 } from 'uuid';
-const authRegister = async (name, email, password, university) => {
+import {makeResponse} from "../utils/response"
+
+export const authRegister = async (name, email, password, university) => {
     const encryptedPassword = await new Promise((resolve, reject) => {
         bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUNDS), function (err, hash) {
             if (err) reject(err);
@@ -19,6 +21,15 @@ const authRegister = async (name, email, password, university) => {
 }
 
 export const authLogin = async (email, password) => {
-    let user = getOneUser(email);
-    if (!user) makeResponse({res, message:"User created successfully"}); 
+    let user = await getOneUser(email);
+    if (!user) return false;
+    const isPasswordMatch = await new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, function (err, hash) {
+            if (err) reject(err);
+            resolve(hash);
+        });
+    })
+    if (!isPasswordMatch) return false;
+    return user;
 }
+
