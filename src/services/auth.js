c72e06107f43b39200b0f266d2e4239c5f21a72c
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-10 17:49:44 +0530

    Implement login service

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt';
-import {createUser} from "../repository/user"
+import {createUser, getOneUser} from "../repository/user"
 import { v4 as uuidv4 } from 'uuid';
-const authRegister = async (name, email, password, university) => {
+import { makeResponse } from '../utils/response';
+export const authRegister = async (name, email, password, university) => {
     const encryptedPassword = await new Promise((resolve, reject) => {
         bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUNDS), function (err, hash) {
             if (err) reject(err);
@@ -18,6 +19,7 @@ const authRegister = async (name, email, password, university) => {
     });
 }
 
-module.exports = {
-    authRegister,
+export const authLogin = async (email, password) => {
+    let user = getOneUser(email);
+    if (!user) makeResponse({res, message:"User created successfully"}); 
 }
