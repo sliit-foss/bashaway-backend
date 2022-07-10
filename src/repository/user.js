edf7da0f221b9d647df2b445764caae93f8ead92
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-10 15:37:14 +0530

    Remove module.export

--- src/repository/user.js
+++ src/repository/user.js
@@ -1,10 +1,6 @@
 import User from "../models/user";
 
-const createUser = async (user) => {
+export const createUser = async (user) => {
     
     return await new User(user).save();
-}
-
-module.exports = {
-    createUser,
 }
