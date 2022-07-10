b7caff3963700cca5f1a1dfa69aa26cd94a208fe
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-10 14:27:30 +0530

    Implement register controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,22 @@
 import asyncHandler from "../middleware/async"
+import {authRegister} from "../services/auth"
+import {makeResponse} from "../utils/response"
+import {sendTokenResponse} from "../utils/jwt";
+import logger from "../utils/logger";
 
-export const register = asyncHandler(async(req, res, next) => {})
+export const register = asyncHandler(async(req, res, next) => {
+    try {
+        const {name, email, password, photo_url} = req.body;
+        
+        const user = await authRegister(name, email, password, photo_url);
+        if (user) return makeResponse({res, message:"User created successfully"});
+        return makeResponse({res, status: 500, message:"Failed to register user"});
+    }
+    catch (e) {
+        logger.error(e);
+        return makeResponse({res, status:500,  message:e.message});
+    }
+});
 
 export const login = asyncHandler(async(req, res, next) => {})
 
