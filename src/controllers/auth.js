52d4ae5adab5605a3fa19af7185c7f70580a6d67
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-10 20:43:02 +0530

    Implement login controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,5 @@
 import asyncHandler from "../middleware/async"
-import {authRegister} from "../services/auth"
+import {authRegister, authLogin} from "../services/auth"
 import {makeResponse} from "../utils/response"
 import {sendTokenResponse} from "../utils/jwt";
 
@@ -13,8 +13,12 @@ export const register = asyncHandler(async(req, res, next) => {
 
 export const login = asyncHandler(async(req, res, next) => {
     const {email, password} = req.body;
-    
-})
+    const user = await authLogin(email, password);
+    if (!user.is_verified) return makeResponse({res, status: 401, message:"User not verified"});
+    if (!user.is_active) return makeResponse({res, status: 401, message:"User not active"});
+    if (user) return sendTokenResponse(res, user, "User logged in successfully");
+    return makeResponse({res, status: 401, message:"Invalid email or password"});   
+});
 
 export const logout = asyncHandler(async(req, res, next) => {})
 
