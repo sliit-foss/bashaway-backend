5722e5a11c71167c5915abd95d3ba5f5549d6ac2
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-10 20:43:50 +0530

    Add middleware to the login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { login, register, logout, current } from '../controllers/auth';
-import { registerSchema } from "../validations/user"
+import { registerSchema, loginSchema } from "../validations/user"
 import { celebrate, Segments } from "celebrate"
 
 const authRouter = express.Router();
 
-authRouter.post('/login', login);
+authRouter.post('/login', celebrate({ [Segments.BODY]: loginSchema }), login);
 authRouter.post('/register', celebrate({ [Segments.BODY]: registerSchema }), register);
 authRouter.get('/logout', logout);
 authRouter.get('/current', current);
