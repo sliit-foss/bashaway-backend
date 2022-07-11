4b90c0379e0b79ebd96b13b009a7bae97358c3bf
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-11 23:17:51 +0530

    Chnage type to number

--- src/models/user.js
+++ src/models/user.js
@@ -66,7 +66,7 @@ const UserSchema = new mongoose.Schema(
           required: true,
         },
         academic_year: {
-          type: String,
+          type: Number,
           required: true,
         }
       }],
