0a8cd3fb75f914caf6d365089414631fe318a2fa
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-11 23:24:40 +0530

    Implement login function (#4)
    * Project init
    
    * Install jsonwebtoken & celebrate
    
    * Implement celebrate error handling
    
    * Implement register controller
    
    * Implement create user repository function
    
    * Implement celebrate middleware
    
    * Implement authRegister service
    
    * Implement jwt functions
    
    * Fix the makeResponse
    
    * Impelement registerSchema
    
    * Add env file
    
    * Add env values
    
    * Install uuid
    
    * Remove Exception handling
    
    * Implement exception handling
    
    * Add new feilds
    
    * Remove module.export
    
    * Update the user document
    
    * Change feilds
    
    * delete env
    
    * Implement login controller
    
    * Implement getone user function
    
    * Implement login service
    
    * Resolve conflits
    
    * Resolve conflicts
    
    * Implement login controller
    
    * Implement getone user function
    
    * Add middleware to the login
    
    * Implement authLogin service function
    
    * Implement login validations
    
    * Add members field
    
    * Add members field to the user model
    
    * Add members field to the authRegister
    
    * Add members field to the validation
    
    * Add members to the user model
    
    * update jwt expire
    
    * Chnage type to number
    
    * Implement member items validations
    
    Co-authored-by: Akalanka <akalankaperera128@gmail.com>

--- src/middleware/async.js
+++ src/middleware/async.js
@@ -1,6 +1,11 @@
+import logger from "../utils/logger";
+
 const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next))
-        .catch(next);
+        .catch((err) => {
+            logger.error(`Error: ${err.message} | Stack: ${err.stack}`);
+            next();
+        });
 }
 
 export default asyncHandler;
