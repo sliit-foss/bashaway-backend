b94439581a61b5a9b57de486e3ab1695f516e25d
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-15 01:34:35 +0530

    Implement a route for the verification

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { login, register, logout, current } from '../controllers/auth';
 import { registerSchema, loginSchema } from "../validations/user"
 import { celebrate, Segments } from "celebrate"
-
+import {verifySuccess} from "../controllers/auth"
 const authRouter = express.Router();
 
 authRouter.post('/login', celebrate({ [Segments.BODY]: loginSchema }), login);
 authRouter.post('/register', celebrate({ [Segments.BODY]: registerSchema }), register);
 authRouter.get('/logout', logout);
 authRouter.get('/current', current);
+authRouter.get("/verify/:id", verifySuccess);
 
 export default authRouter;
