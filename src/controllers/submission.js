2cea58de5ba24994c01fd214556c66813a5aea12
Shifan <76872645+QbicSquid@users.noreply.github.com>
2022-07-14 09:55:49 +0530

    Feature/grade submission (#7)
    * Changed routes for view submission
    
    * Added submission grading functionality
    
    * Set initial submission score to null
    
    * Removed obsolete data from submission http responses

--- src/controllers/submission.js
+++ src/controllers/submission.js
@@ -1,15 +1,21 @@
 import asyncHandler from '../middleware/async'
-import { createSubmission, viewAllSubmissions } from '../services/submission'
+import { createSubmission, viewSubmissions, gradeSubmission } from '../services/submission'
 import { makeResponse } from '../utils/response'
 
 export const create = asyncHandler(async (req, res, next) => {
   await createSubmission(req.body)
 
-  makeResponse({ res, status: 200, data: {}, message: 'submission created' })
+  makeResponse({ res, status: 201, message: 'submission created' })
 })
 
-export const viewAll = asyncHandler(async (req, res, next) => {
-  const data = await viewAllSubmissions(req.query)
+export const view = asyncHandler(async (req, res, next) => {
+  const data = await viewSubmissions(req.query)
 
   makeResponse({ res, status: 200, data, message: 'submissions retrieved' })
+})
+
+export const grade = asyncHandler(async (req, res, next) => {
+  await gradeSubmission(req.body)
+
+  makeResponse({ res, status: 201, message: 'submission graded' })
 })
--- src/routes/submission.routes.js
+++ src/routes/submission.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { create, viewAll } from '../controllers/submission'
+import { create, view, grade } from '../controllers/submission'
 
 const submissionRouter = express.Router()
 
 submissionRouter.post('/create', create)
-submissionRouter.get('/viewAll', viewAll)
+submissionRouter.post('/grade', grade)
+submissionRouter.get('/view', view)
 
 export default submissionRouter
