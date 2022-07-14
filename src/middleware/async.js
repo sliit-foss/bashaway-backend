19655a882f45f4eb9ca8c63fa2c34b21856865a2
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-15 01:32:49 +0530

    Change asyncHandler function

--- src/middleware/async.js
+++ src/middleware/async.js
@@ -2,10 +2,7 @@ import logger from "../utils/logger";
 
 const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next))
-        .catch((err) => {
-            logger.error(`Error: ${err.message} | Stack: ${err.stack}`);
-            next();
-        });
+        .catch(next);
 }
 
 export default asyncHandler;
