cf1ce457f4f5c87e429124637b3ec914bb41524c
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-07-15 01:35:14 +0530

    Implement verifyTemplate & updateVerify functions

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,10 @@
 import bcrypt from 'bcrypt';
-import { createUser, getOneUser } from "../repository/user"
+const fs = require('fs');
 import { v4 as uuidv4 } from 'uuid';
+import {createUser, getOneUser, updateVerifiedUser, getUserByCode} from "../repository/user"
+import {sendVerificationMail} from "./email";
 
-export const authRegister = async ({ name, email, password, university, members }) => {
+export const authRegister = async (name, email, password, university, members) => {
     const user = await getOneUser({ email });
     if (user) return { status: 400, message: 'User already exists' };
     const encryptedPassword = await new Promise((resolve, reject) => {
@@ -11,14 +13,19 @@ export const authRegister = async ({ name, email, password, university, members 
             resolve(hash);
         });
     });
-    return await createUser({
+
+    const verification_code = uuidv4();
+    const registeredUser =  await createUser({
         name,
         email,
         password: encryptedPassword,
-        verification_code: uuidv4(),
+        verification_code: verification_code,
         university,
         members,
     });
+    await verifyMailTemplate(email, verification_code);
+
+    return registeredUser;
 }
 
 export const authLogin = async ({ email, password }) => {
@@ -34,3 +41,29 @@ export const authLogin = async ({ email, password }) => {
     return user;
 }
 
+export const verifyMailTemplate = async (email, verification_code) => {
+
+    const html = fs.readFileSync(__basedir + "/html/emailTemplate.html", "utf8");
+    let replacements = {
+        verify_URL: `http://localhost:3001/api/auth/verify/${verification_code}`,
+    }
+    let subject = "Welcome to the Bashaway"
+
+    await sendVerificationMail(email, html, replacements, subject);
+    return true;
+}
+
+export const updateVerify = async (id) => {
+
+    const user = await getUserByCode(id);
+    if (!user) return false;
+    return await updateVerifiedUser({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        verification_code: user.verification_code,
+        is_verified: true,
+        university: user.university,
+        members: user.members,
+    });
+}
