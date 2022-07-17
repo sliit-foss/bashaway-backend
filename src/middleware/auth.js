382cffaee3b6503e8b2d6239facfe9442c3c0fa6
QbicSquid <shifan611@gmail.com>
2022-07-17 22:34:04 +0530

    JWT Token generation issue fixed.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,21 +4,21 @@ import { makeResponse } from '../utils/response'
 import asyncHandler from './async'
 
 export const protect = asyncHandler(async (req, res, next) => {
-    let token = req.headers.authorization
-        ? req.headers.authorization.startsWith('Bearer')
-            ? req.headers.authorization.split(' ')[1]
-            : null
-        : null
-    if (!token && req.cookies.token) token = req.cookies.token
-    if (!token) return makeResponse({ res, status: 403, message: 'Unauthorized' })
-    const decodedUser = decodeJwtToken(token).data
-    const user = decodedUser ? await getOneUser({ _id: decodedUser._id }) : null
-    if (!user) return makeResponse({ res, status: 403, message: 'Unauthorized' })
-    req.user = user
-    next()
+  let token = req.headers.authorization
+    ? req.headers.authorization.startsWith('Bearer')
+      ? req.headers.authorization.split(' ')[1]
+      : null
+    : null
+  if (!token && req.cookies.token) token = req.cookies.token
+  if (!token) return makeResponse({ res, status: 403, message: 'Unauthorized' })
+  const decodedUser = decodeJwtToken(token).data
+  const user = decodedUser ? await getOneUser({ _id: decodedUser._id }) : null
+  if (!user) return makeResponse({ res, status: 403, message: 'Unauthorized' })
+  req.user = user
+  next()
 })
 
 export const adminProtect = asyncHandler(async (req, res, next) => {
-    if (req.user.role !== 'ADMIN') return makeResponse({ res, status: 403, message: 'Unauthorized' })
-    next()
+  if (req.user.role !== 'ADMIN') return makeResponse({ res, status: 403, message: 'Unauthorized' })
+  next()
 })
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,33 +1,37 @@
-import jwt from "jsonwebtoken";
-import { v4 as uuidv4 } from 'uuid';
-import { findOneAndUpdateUser } from "../repository/user";
+import jwt from 'jsonwebtoken'
+import { v4 as uuidv4 } from 'uuid'
+import { findOneAndUpdateUser } from '../repository/user'
 
 export const sendTokenResponse = async (res, user, message) => {
-    const accessToken = generateToken(user)
-    const refreshToken = generateToken(uuidv4())
+  const accessToken = generateToken(user)
+  const refreshToken = generateToken(uuidv4())
 
-    await findOneAndUpdateUser({ _id: user._id }, { refresh_token: refreshToken })
+  await findOneAndUpdateUser({ _id: user._id }, { refresh_token: refreshToken })
 
-    const options = {
-        expires: new Date(Date.now() + process.env.JWT_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly: true
-    }
+  const options = {
+    expires: new Date(Date.now() + process.env.JWT_EXPIRE * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  }
 
-    if (process.env.NODE_ENV === "production") {
-        options.secure = true
-    }
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true
+  }
 
-    res.status(200).cookie("token", accessToken, options).json({
-        data: { user, access_token: accessToken, refresh_token: refreshToken }, message
+  res
+    .status(200)
+    .cookie('token', accessToken, options)
+    .json({
+      data: { user, access_token: accessToken, refresh_token: refreshToken },
+      message,
     })
 }
 
 export const generateToken = (user) => {
-    return jwt.sign({ data: user }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE,
-    });
+  return jwt.sign({ data: user }, process.env.JWT_SECRET, {
+    expiresIn: `${process.env.JWT_EXPIRE}d`,
+  })
 }
 
 export const decodeJwtToken = (token) => {
-    return jwt.verify(token, process.env.JWT_SECRET)
+  return jwt.verify(token, process.env.JWT_SECRET)
 }
