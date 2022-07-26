e546588eb45083aa70e737b86adace906ac50d51
QbicSquid <shifan611@gmail.com>
2022-07-26 17:16:57 +0530

    Added body validations for submission creation

--- src/routes/submission.routes.js
+++ src/routes/submission.routes.js
@@ -3,11 +3,11 @@ import { celebrate, Segments } from 'celebrate'
 
 import { create, view, grade } from '../controllers/submission'
 import { adminProtect } from '../middleware/auth'
-import { submissionIdSchema, submissionViewSchema } from '../validations/submission'
+import { submissionIdSchema, submissionViewSchema, submissionCreateSchema } from '../validations/submission'
 
 const submissionRouter = express.Router()
 
-submissionRouter.post('/', create)
+submissionRouter.post('/', celebrate({ [Segments.BODY]: submissionCreateSchema }), create)
 submissionRouter.get('/', celebrate({ [Segments.QUERY]: submissionViewSchema}), view)
 submissionRouter.put('/:id', celebrate({ [Segments.PARAMS]: submissionIdSchema }), adminProtect, grade)
 
