944b9e68adefd0f6161fd11e303682887dbac48f
QbicSquid <shifan611@gmail.com>
2022-07-26 21:37:47 +0530

    Added celebrate ID validations to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,29 @@
-import express from 'express';
-import { celebrate, Segments } from "celebrate"
-import { create, getAll, getById, update, updateScore, updateAllScores, changePassword } from '../controllers/user';
-import { adminProtect } from '../middleware/auth';
-import { addUserSchema, changePasswordSchema } from '../validations/user';
+import express from 'express'
+import { celebrate, Segments } from 'celebrate'
+import {
+  create,
+  getAll,
+  getById,
+  update,
+  updateScore,
+  updateAllScores,
+  changePassword,
+} from '../controllers/user'
+import { adminProtect } from '../middleware/auth'
+import { addUserSchema, changePasswordSchema, userIdSchema } from '../validations/user'
 
-const userRouter = express.Router();
+const userRouter = express.Router()
 
-userRouter.post('/', adminProtect, celebrate({ [Segments.BODY]: addUserSchema }), create);
-userRouter.get('/', adminProtect, getAll);
-userRouter.get('/:id', adminProtect, getById);
-userRouter.put('/score', updateAllScores);
-userRouter.put('/change_password', celebrate({ [Segments.BODY]: changePasswordSchema }), changePassword);
-userRouter.put('/:id', adminProtect, update);
-userRouter.put('/:id/score', adminProtect, updateScore);
+userRouter.post('/', adminProtect, celebrate({ [Segments.BODY]: addUserSchema }), create)
+userRouter.get('/', adminProtect, getAll)
+userRouter.get('/:id', celebrate({ [Segments.PARAMS]: userIdSchema }), adminProtect, getById)
+userRouter.put('/score', updateAllScores)
+userRouter.put(
+  '/change_password',
+  celebrate({ [Segments.BODY]: changePasswordSchema }),
+  changePassword,
+)
+userRouter.put('/:id', adminProtect, celebrate({ [Segments.PARAMS]: userIdSchema }), update)
+userRouter.put('/:id/score', adminProtect, celebrate({ [Segments.PARAMS]: userIdSchema }), updateScore)
 
-export default userRouter;
+export default userRouter
