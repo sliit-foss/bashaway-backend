42af2462ec0629bf7bdc1c31a4bc00d108adcbb1
Shifan <76872645+QbicSquid@users.noreply.github.com>
2022-07-26 17:21:36 +0530

    Fix/submission bad responses (#37)
    * Submission grading error responses fixed
    
    * Added query param validations for submission viewing
    
    * Added body validations for submission creation

--- src/routes/submission.routes.js
+++ src/routes/submission.routes.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import { celebrate, Segments } from 'celebrate'
+
 import { create, view, grade } from '../controllers/submission'
-import { adminProtect } from '../middleware/auth';
+import { adminProtect } from '../middleware/auth'
+import { submissionIdSchema, submissionViewSchema, submissionCreateSchema } from '../validations/submission'
 
 const submissionRouter = express.Router()
 
-submissionRouter.post('/', create)
-submissionRouter.get('/', view)
-submissionRouter.put('/:id', adminProtect, grade)
+submissionRouter.post('/', celebrate({ [Segments.BODY]: submissionCreateSchema }), create)
+submissionRouter.get('/', celebrate({ [Segments.QUERY]: submissionViewSchema}), view)
+submissionRouter.put('/:id', celebrate({ [Segments.PARAMS]: submissionIdSchema }), adminProtect, grade)
 
 export default submissionRouter
--- src/services/submission.js
+++ src/services/submission.js
@@ -16,17 +16,21 @@ export const viewSubmissions = async (query) => {
 
 export const gradeSubmission = async (submissionId, { score }, { _id }) => {
   const submission = await getSubmissionById(submissionId)
+  if (!submission) return {
+    status: 422,
+    message: 'Error: Invalid submission ID'
+  }
   const maxScore = await getMaxScore(submission.question.toString())
 
   if (score < 0)
     return {
-      status: 400,
-      message: 'Error: score must be greater than or equal to 0',
+      status: 422,
+      message: 'Error: Score must be greater than or equal to 0',
     }
   else if (maxScore < score)
     return {
-      status: 400,
-      message: 'Error: score must be less than or equal to the max score for the question',
+      status: 422,
+      message: 'Error: Score must be less than or equal to the max score for the question',
     }
 
   await insertGrade(submissionId, score, _id)
