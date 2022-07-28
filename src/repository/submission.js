592eb8b037ccec80029d478e9c6b9cbe284cfaca
QbicSquid <shifan611@gmail.com>
2022-07-29 00:09:52 +0530

    Fixed submission sorting when grading users

--- src/repository/submission.js
+++ src/repository/submission.js
@@ -46,7 +46,8 @@ export const getLatestScore = async ({ user, question }) => {
   const sort = {
     created_at: 'desc',
   }
-  const result = await Submission.findOne(filters).setOptions(sort).lean()
+  const result = await Submission.findOne(filters).setOptions({ sort }).lean()
+  console.log(result)
   if (result) return result.score
   else return 0
 }
