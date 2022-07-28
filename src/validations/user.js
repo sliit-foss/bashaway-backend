aede34b9f61949da28fd144c7ce883f0bd95cb44
Thulina Wickramasinghe <thulinawickramasinghe@gmail.com>
2022-07-28 23:54:53 +0530

    Added validation for academic year (#43)
    * Added validation for academic year
    
    Academic year should be from 1 to 4
    
    * Added joi validations for academic year

--- src/validations/user.js
+++ src/validations/user.js
@@ -13,7 +13,7 @@ export const registerSchema = Joi.object({
         name: Joi.string().required(),
         email: Joi.string().email().required(),
         phone: Joi.number().required(),
-        academic_year: Joi.number().required()
+        academic_year: Joi.number().required().min(1).max(4)
     })).max(4).optional(),
 });
 
