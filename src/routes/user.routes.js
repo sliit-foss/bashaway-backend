069097205aee1e3d6ccd130c6230306788098a09
Supuni Eleesha Randeniya <supuni.randeniya@gmail.com>
2022-08-14 13:15:59 +0530

    Allow user score update only for admins

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,20 +10,34 @@ import {
   changePassword,
 } from '../controllers/user'
 import { adminProtect } from '../middleware/auth'
-import { addUserSchema, changePasswordSchema, userIdSchema , updateSchema } from '../validations/user'
+import {
+  addUserSchema,
+  changePasswordSchema,
+  userIdSchema,
+  updateSchema,
+} from '../validations/user'
 
 const userRouter = express.Router()
 
 userRouter.post('/', adminProtect, celebrate({ [Segments.BODY]: addUserSchema }), create)
 userRouter.get('/', adminProtect, getAll)
 userRouter.get('/:id', celebrate({ [Segments.PARAMS]: userIdSchema }), adminProtect, getById)
-userRouter.put('/score', updateAllScores)
+userRouter.put('/score', adminProtect, updateAllScores)
 userRouter.put(
   '/change_password',
   celebrate({ [Segments.BODY]: changePasswordSchema }),
   changePassword,
 )
-userRouter.put('/:id', celebrate({ [Segments.PARAMS]: userIdSchema , [Segments.BODY]: updateSchema }), update)
-userRouter.put('/:id/score', adminProtect, celebrate({ [Segments.PARAMS]: userIdSchema }), updateScore)
+userRouter.put(
+  '/:id',
+  celebrate({ [Segments.PARAMS]: userIdSchema, [Segments.BODY]: updateSchema }),
+  update,
+)
+userRouter.put(
+  '/:id/score',
+  adminProtect,
+  celebrate({ [Segments.PARAMS]: userIdSchema }),
+  updateScore,
+)
 
 export default userRouter
