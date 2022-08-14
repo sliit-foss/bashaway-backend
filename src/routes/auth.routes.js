4d4a74ab4d36bb32cc9f3dd357d6f2196003cf26
Sayumi Munasinghe <it20610920@my.sliit.lk>
2022-08-14 11:31:58 +0530

    Added requested changes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { celebrate, Segments } from "celebrate"
 import { login, register, current, verifyUser, resendVerification, forgotPassword, resetPassword } from '../controllers/auth';
-import { registerSchema, loginSchema, verifySchema, resetPasswordSchema } from "../validations/user"
+import { registerSchema, loginSchema, verifySchema, validUserResetPasswordSchema, resetPasswordSchema } from "../validations/user"
 import { protect } from '../middleware/auth';
 
 const authRouter = express.Router();
@@ -12,5 +12,5 @@ authRouter.get('/current', protect, current);
 authRouter.post('/verify', resendVerification);
 authRouter.get("/verify/:verification_code", celebrate({ [Segments.PARAMS]: verifySchema }), verifyUser);
 authRouter.post('/forgot_password', forgotPassword);
-authRouter.post('/reset_password/:verification_code', celebrate({ [Segments.BODY]: resetPasswordSchema }), resetPassword);
+authRouter.post('/reset_password/:verification_code', celebrate({ [Segments.PARAMS]: validUserResetPasswordSchema, [Segments.BODY]: resetPasswordSchema }, ), resetPassword);
 export default authRouter;
