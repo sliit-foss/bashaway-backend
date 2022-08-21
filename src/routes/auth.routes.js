f5055e0499451bb0b69e25b84715e7318e17a82c
QbicSquid <shifan611@gmail.com>
2022-08-21 11:47:30 +0530

    Added extra rate limiter to resend verify email

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,13 +3,21 @@ import { celebrate, Segments } from "celebrate"
 import { login, register, current, verifyUser, resendVerification, forgotPassword, resetPassword } from '../controllers/auth';
 import { registerSchema, loginSchema, verifySchema, validUserResetPasswordSchema, resetPasswordSchema } from "../validations/user"
 import { protect } from '../middleware/auth';
+import rateLimit from 'express-rate-limit';
 
 const authRouter = express.Router();
+
+const verifyEmailLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 2, // 2 requests per 1 minute allowed
+  standardHeaders: true,
+  legacyHeaders: false,
+})
 
 authRouter.post('/login', celebrate({ [Segments.BODY]: loginSchema }), login);
 authRouter.post('/register', celebrate({ [Segments.BODY]: registerSchema }), register);
 authRouter.get('/current', protect, current);
-authRouter.post('/verify', resendVerification);
+authRouter.post('/verify',celebrate(verifyEmailLimiter ,resendVerification);
 authRouter.get("/verify/:verification_code", celebrate({ [Segments.PARAMS]: verifySchema }), verifyUser);
 authRouter.post('/forgot_password', forgotPassword);
 authRouter.post('/reset_password/:verification_code', celebrate({ [Segments.PARAMS]: validUserResetPasswordSchema, [Segments.BODY]: resetPasswordSchema }, ), resetPassword);
