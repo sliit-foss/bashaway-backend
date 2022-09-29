8e8aaabd8769689b3a79604f96f9b54afb7e02e7
Akalanka <akalankaperera128@gmail.com>
2022-09-30 01:17:51 +0530

    Fixed issue in question update

--- src/services/question.js
+++ src/services/question.js
@@ -31,7 +31,7 @@ export const updateQuestionById = async (question_id, data, user) => {
   if (!question) return { status: 400, message: "Question doesn't exist to update" }
   if (data.name) {
     const check = await findQuestion({ name: data.name })
-    if (check) return { status: 400, message: 'Question name already taken' }
+    if (check && check._id?.toString() !== question_id?.toString()) return { status: 400, message: 'Question name already taken' }
   }
   if (question.creator_lock && question.creator.toString() !== user._id.toString()) return { status: 403, message: 'You are not authorized to update this question' }
   if (data.max_score) {
