1baeaa216950b1d15f8b656df9a06053f93da576
Akalanka <akalankaperera128@gmail.com>
2023-07-29 10:17:16 +0530

    Fix: auth middleware error statuses

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,16 +9,17 @@ export const protect = asyncHandler(async (req, res) => {
       ? req.headers.authorization.split(' ')[1]?.replace('null', '')?.replace('undefined', '')
       : null
     : null;
-  if (!token) return makeResponse({ res, status: 403, message: 'Unauthorized' });
+  if (!token) return makeResponse({ res, status: 401, message: 'Unauthorized' });
   const isBackListedToken = isBlacklistedToken(token);
-  if (isBackListedToken) return makeResponse({ res, status: 403, message: 'Unauthorized' });
+  if (isBackListedToken) return makeResponse({ res, status: 401, message: 'Unauthorized' });
   const decodedUser = decodeJwtToken(token).data;
   const user = decodedUser ? await getOneUser({ _id: decodedUser._id }, false) : null;
-  if (!user) return makeResponse({ res, status: 403, message: 'Unauthorized' });
+  if (!user) return makeResponse({ res, status: 401, message: 'Unauthorized' });
   req.user_token = token;
   req.user = user;
 });
 
 export const adminProtect = asyncHandler((req, res) => {
-  if (req.user.role !== 'ADMIN') return makeResponse({ res, status: 403, message: 'Unauthorized' });
+  if (req.user.role !== 'ADMIN')
+    return makeResponse({ res, status: 403, message: 'You are not permitted to access this resource' });
 });
