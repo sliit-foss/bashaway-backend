2dcdc1f501f2758cb6577639125ed468362e6396
Akalanka <akalankaperera128@gmail.com>
2023-09-01 00:56:25 +0530

    Patch: url signing

--- src/controllers/storage.js
+++ src/controllers/storage.js
@@ -2,7 +2,7 @@ import { signUrl as sign } from '@/services/storage';
 import { makeResponse } from '@/utils/response';
 
 export const signUrl = (req, res) => {
-  const signedUrl = sign(decodeURIComponent(req.query.url), req.query.upload === 'true');
+  const signedUrl = sign(decodeURIComponent(req.query.url), req.query.upload);
   return makeResponse({
     res,
     data: { signed_url: signedUrl },
