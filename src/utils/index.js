d4585524b4421f5049150ac7137c9c01e0beb15d
Akalanka <akalankaperera128@gmail.com>
2023-09-02 00:07:47 +0530

    Chore: added todo above raw repo url constant

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,6 +8,7 @@ export const isFromAdmin = () => context.get('origin') === 'admin';
 
 export const isProduction = process.env.APP_ENV === 'production';
 
+// TODO: Replace this with your own repo if you are forking this project
 export const rawRepoUrl = `https://raw.githubusercontent.com/sliit-foss/bashaway-backend/${
   isProduction ? 'main' : 'development'
 }`;
