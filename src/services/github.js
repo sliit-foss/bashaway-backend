0450d76651dd90c248407a950def076fc33b3e79
Akalanka <akalankaperera128@gmail.com>
2023-09-01 22:54:25 +0530

    Fix: scorekeeper api request

--- src/services/github.js
+++ src/services/github.js
@@ -12,11 +12,14 @@ export const triggerScorekeeper = (email, submissionId, submissionLink, question
   return connector.post(
     `/repos/${process.env.SCOREKEEPER_REPO_OWNER}/${process.env.SCOREKEEPER_REPO_NAME}/dispatches`,
     {
-      email,
-      submission_id: submissionId,
-      submission_url: submissionLink,
-      question_url: questionLink,
-      strict_inputs: strictInputs
+      event_type: 'run-tests',
+      client_payload: {
+        email,
+        submission_id: submissionId,
+        submission_url: submissionLink,
+        question_url: questionLink,
+        strict_inputs: strictInputs
+      }
     }
   );
 };
