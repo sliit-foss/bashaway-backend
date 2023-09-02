b5dd686d38ff2c61fd7d993b596879dca087c6ba
Akalanka <akalankaperera128@gmail.com>
2023-09-02 14:32:27 +0530

    Chore: updated scorekeeper trigger event type

--- src/services/github.js
+++ src/services/github.js
@@ -12,14 +12,13 @@ export const triggerScorekeeper = (email, submissionId, submissionLink, question
   return connector.post(
     `/repos/${process.env.SCOREKEEPER_REPO_OWNER}/${process.env.SCOREKEEPER_REPO_NAME}/dispatches`,
     {
-      event_type: 'run-tests',
+      event_type: `run-${process.env.APP_ENV}-tests`,
       client_payload: {
         email,
         submission_id: submissionId,
         submission_url: submissionLink,
         question_url: questionLink,
-        strict_inputs: strictInputs,
-        app_env: process.env.APP_ENV
+        strict_inputs: strictInputs
       }
     }
   );
