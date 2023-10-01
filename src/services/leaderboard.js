532abe772680be704c4ce248f2de76682b0beeb1
Akalanka <akalankaperera128@gmail.com>
2023-10-01 23:18:10 +0530

    Fix: unique freezed leaderboard names

--- src/services/leaderboard.js
+++ src/services/leaderboard.js
@@ -8,12 +8,18 @@ export const getLeaderboardRankings = async () => {
   const records = await getLeaderboardData();
   if (freezed) {
     const freezeDate = new Date(freezeAt);
+    const names = new Set();
     for (let i = 0; i < records.length; i++) {
       if (i >= freezeVanguard) {
         break;
       }
       if (new Date() > freezeDate) {
-        const name = faker.science.chemicalElement().name;
+        let name = faker.science.chemicalElement().name;
+        while (names.has(name)) {
+          name = faker.science.chemicalElement().name;
+          names.add(name);
+        }
+        records[i].name = name;
         records[i].score = '????';
         records[i].name = name;
         records[i].university = startCase(faker.science.chemicalElement().symbol.toUpperCase());
